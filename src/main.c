59f1623f8d36bcefa7bb6f488d55c4267da1f807
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2022-04-16 21:31:04 -0700

    Add more functions

--- src/main.c
+++ src/main.c
@@ -1,19 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define SIZE 1000
+#define SIZE 100000
 #define RADIUS 100
 
-int* X[SIZE];
-int* Y[SIZE];
+int X[SIZE];
+int Y[SIZE];
+
+void add_nums(int *array) {
+    int random;
+
+    for (int i = 0; i < SIZE; i++) {
+        random = rand() % RADIUS;
+        array[i] = random;
+    }
+}
+
+void print_array(int *array) {
+    int c;
+
+    while (c != '\0') {
+        c = array[i];
+        printf("%d\n", array[i]);
+    }
+}
 
 int main() {
-	unsigned int seed;
-	srand(seed);
+    int i;
+    unsigned int seed;
 
-	for (int i = 0; i < SIZE; i++) {
-		int random = rand() % RADIUS;
-		printf("%d\n", random);
-	}
+    srand(seed);
 
-	return 0;
+    add_nums(&X);
+    print_array(&X);
+
+    return 0;
 }
